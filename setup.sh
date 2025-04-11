08b6ba0bc1e84eea7ee2b6aec2807a1fa9174989
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2025-04-11 19:37:10 +0200

    Modify setup.sh to handle execution cases

--- setup.sh
+++ setup.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    echo "Installing required dependencies for MacOS..."
-    if ! command -v brew &>/dev/null; then
-        echo "🍺 Homebrew not found. Installing..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    fi
-    brew install cmake m4 tbb
+setup_macos() {
+    brew install cmake m4 tbb wget gnu-tar
     brew install --cask xquartz
     if ! xcode-select -p &>/dev/null; then
         echo "Xcode command line tools not found. Installing..."
         xcode-select --install
     fi
-fi
+    export PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
+    ./get-inputs
+}
 
+echo "Cloning latest version of parsec-benchmark repository..."
 TARGET_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$TARGET_DIRECTORY"
-git clone https://github.com/cirosantilli/parsec-benchmark.git
+git clone https://github.com/cirosantilli/parsec-benchmark.git && cd parsec-benchmark
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Installing required dependencies for MacOS..."
+    setup_macos
+else
+    echo "Assuming FinisTerrae III..."
+    if ! command -v module &>/dev/null || ! module load cesga/system; then
+        echo "Defaulting to original setup script"
+        ./configure
+    else
+        ./get-inputs
+    fi
+fi
